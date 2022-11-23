ff63a4836b524cf85d5c3460bc3a840ff095d974
Im_Rukshan <rukshan.maduranga@hatchyard.io>
2022-11-23 14:59:44 +0530

    styling the background

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import {
     Platform,
     StatusBar,
 } from "react-native";
+import { colors } from "./src/utils/colors";
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Test 1</Text>
+            <Text style={styles.text}>Test 1</Text>
         </SafeAreaView>
     );
 }
@@ -18,5 +19,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        backgroundColor: colors.darkBlue,
+    },
+    text: {
+        color: colors.white,
     },
 });
