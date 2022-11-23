b824d45cc826fb4820387a62cf515f31b3ac48c1
Im_Rukshan <rukshan.maduranga@hatchyard.io>
2022-11-23 20:39:27 +0530

    storing the subject

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,22 +1,25 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { colors } from "../utils/colors";
 import { TextInput } from "react-native-paper";
 
-export const Focus = () => (
-    <View style={styles.container}>
-        <View style={styles.inputContainer}>
-            <TextInput label="What would you like to focus on" />
+export const Focus = () => {
+    const [subject, setSubject] = useState(null);
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    onChangeText={setSubject}
+                    label="What would you like to focus on"
+                />
+            </View>
         </View>
-    </View>
-);
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    text: {
-        color: colors.white,
     },
     inputContainer: {
         flex: 0.5,
