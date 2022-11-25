5ed35d3f4116c36928c2e102d174e4c67798a38b
Im_Rukshan <rukshan.maduranga@hatchyard.io>
2022-11-25 12:38:46 +0530

    populate the focus history

--- App.js
+++ App.js
@@ -10,17 +10,24 @@ import { colors } from "./src/utils/colors";
 import { Focus } from "./src/features/Focus";
 import { useState } from "react";
 import { Timer } from "./src/features/Timer";
+import { FocusHistory } from "./src/features/FocusHistory";
 
 export default function App() {
-    const [currentSubject, setCurrentSubject] = useState("test");
+    const [currentSubject, setCurrentSubject] = useState();
+    const [history, setHistory] = useState([]);
     return (
         <SafeAreaView style={styles.container}>
             {!currentSubject ? (
-                <Focus addSubject={setCurrentSubject} />
+                <>
+                    <Focus addSubject={setCurrentSubject} />
+                    <FocusHistory history={history} />
+                </>
             ) : (
                 <Timer
                     focusSubject={currentSubject}
-                    onTimerEnd={() => {}}
+                    onTimerEnd={(subject) => {
+                        setHistory([...history, subject]);
+                    }}
                     clearSubject={() => {
                         setCurrentSubject(null);
                     }}
